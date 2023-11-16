14bd0c3474b5da3bbf32a29deeacba0e0b4615d4
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2023-11-16 14:40:18 +0100

    Add files via upload

---
+++ lab7/KodKlass.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * @author local
+ * @version 1.0
+ * @created 16-lis-2023 14:37:30
+ */
+public class Klient {
+
+	private char nazwisko;
+	private int nr_klienta;
+	public Towar jest_wyszukiwany;
+
+	public Klient(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void CRUD_klient(){
+
+	}
+}//end Klient
